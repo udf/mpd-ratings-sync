b9c4c3e08739c1d1b2c328344406c8bf1c125767
udf <tabhooked@gmail.com>
2025-03-24 23:34:00 +0200

    print sources for ratings

--- load_ratings.py
+++ load_ratings.py
@@ -1,6 +1,7 @@
 import glob
 import os
 from collections import defaultdict
+from pathlib import Path
 
 from common import mpd_client, RATINGS_DB, load_db, RatingValue
 
@@ -22,9 +23,11 @@ default_file_rating = RatingValue(0, 0)
 # find all ratings that are newer than our ratings
 our_ratings = strip_rating_exts(load_db(RATINGS_DB))
 changed_ratings = {}
+path_src = {}
 for db_file in glob.glob('*_ratings.sql'):
   if db_file == RATINGS_DB:
     continue
+  db_shortname = Path(db_file).stem
   for path, rating in load_db(db_file).items():
     path = strip_ext(path)
     current_rating = changed_ratings.get(
@@ -33,6 +36,7 @@ for db_file in glob.glob('*_ratings.sql'):
     )
     if rating.updated > current_rating.updated and rating.rating != current_rating.rating:
       changed_ratings[path] = rating
+      path_src[path] = db_shortname
 
 if changed_ratings:
   print(len(changed_ratings), 'rating(s) to update')
@@ -46,7 +50,7 @@ find_files = lambda path: path_mapping[path]
 for path, rating in changed_ratings.items():
   real_paths = find_files(path)
   for real_path in real_paths:
-    print(f'rating={rating.rating}', real_path)
+    print(f'rating={rating.rating}', real_path, path_srcs.get(path))
     mpd_client.sticker_set('song', real_path, 'rating', rating.rating)
   if not real_paths:
-    print('Not found', f'rating={rating.rating}', path)
+    print('Not found', f'rating={rating.rating}', path, {path_src.get(path)})
